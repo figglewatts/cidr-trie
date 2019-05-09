c4aeb1c0132ebf41555b2170b2462f0068fd4eab
Figglewatts <me@figglewatts.co.uk>
2019-05-09 16:15:33 +0100

    Fixed problem with RTD build

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,8 @@ source_suffix = ['.rst']
 #
 html_theme = 'alabaster'
 
+master_doc = 'index'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
