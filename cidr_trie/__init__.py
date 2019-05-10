161f4378095d92aaeb895523a1fc04c7848f9f60
Figglewatts <me@figglewatts.co.uk>
2019-05-10 13:25:37 +0100

    Modified the way values are stored, allowing for multiple netmasks to be stored on a single node

--- cidr_trie/__init__.py
+++ cidr_trie/__init__.py
@@ -21,7 +21,7 @@ A Patricia trie can be created, inserted to, and searched like this
 
 from .cidr_util import is_v6, cidr_atoi, longest_common_prefix_length, get_subnet_mask, ip_itoa
 from .bits_util import is_set, ffs
-from typing import Any, List
+from typing import Any, List, Dict, Tuple
 
 
 class PatriciaNode:
@@ -29,21 +29,48 @@ class PatriciaNode:
 
     Attributes:
         ip (int): The IP address associated with this node.
-        mask (int): The netmask associated with this node.
         bit (int): How many bits along the IP the decision is made to branch.
-        value (Any): The data stored on this node.
+        value (Dict[int, Any]): The data stored on this node. Maps netmasks to data.
         parent (PatriciaNode): The parent of this node.
         left (PatriciaNode): The left subtrie of this node.
         right (PatriciaNode): The right subtrie of this node.
     """
-    def __init__(self, ip: int=0, mask: int=0, bit: int=0, data: Any=None) -> None:
+    def __init__(self, ip: int=0, bit: int=0, data: Dict[int, Any]={}) -> None:
         self.ip = ip
-        self.mask = mask
         self.bit = bit
         self.value = data
         self.parent = None
         self.left = None
         self.right = None
+
+    def get_values(self, prefix: str) -> Dict[str, Any]:
+        """Get values from this node by iterating through netmasks and
+        checking to see if the given prefix is contained within.
+
+        Args:
+            prefix: The prefix to use to check, i.e. "192.168.0.0/16"
+        Returns:
+            Dict[str, Any]: dict mapping prefixes to values, i.e. {"192.168.0.0/16": 2856}
+        """
+        # parse the CIDR string
+        ip, mask = cidr_atoi(prefix)
+        v6 = is_v6(prefix)
+        result = {}
+
+        # for each mask stored in this node, check to see if the IP given
+        # by the prefix masked by the mask is equal to the IP stored in the node
+        # this indicates that this IP is within the network defined by the node
+        # IP and particular netmask
+        for m in self.value.keys():
+            # if the mask is greater than the given mask, there's no way the prefix
+            # can be in this range, as it's bigger than this network
+            if m > mask:
+                continue
+
+            if self.ip == (ip & get_subnet_mask(m, v6)):
+                result[f"{ip_itoa(self.ip, False)}/{m}"] = self.value[m]
+
+        return result
 
 
 class PatriciaTrie:
@@ -74,12 +101,12 @@ class PatriciaTrie:
 
     """
     def __init__(self) -> None:
-        self.root = PatriciaNode(0, 0, 0)
+        self.root = PatriciaNode(0, 0)
         self.v6 = False
         self.size = 0
 
-    def insert(self, prefix: str, value: Any) -> None:
-        """Insert an IP and value into the trie.
+    def insert(self, prefix: str, data: Any) -> None:
+        """Insert an IP and data into the trie.
 
         If the IP was already in the trie it will overwrite the value.
 
@@ -90,7 +117,7 @@ class PatriciaTrie:
 
         Args:
             prefix: The prefix to insert, i.e. "192.168.0.0/16"
-            value: The value to associate with the IP.
+            data: The value to associate with the IP and netmask.
 
         Raises:
             ValueError: When trying to store an IPv4 address in a trie currently storing IPv6 addresses, and vice-versa.
@@ -121,7 +148,7 @@ class PatriciaTrie:
 
         # check to see if the last node visited was a match
         if last_node.ip == ip:
-            last_node.value = value
+            last_node.value[mask] = data
             return
 
         # it wasn't an exact match, so we need to figure out where to
@@ -150,7 +177,7 @@ class PatriciaTrie:
         # we've now found a node with a bit lower than the LCP,
         # indicating that it's a valid prefix of the current IP
         # insert the new node on a subtrie of the found node
-        to_insert = PatriciaNode(ip, mask, rightmost_set_bit, value)
+        to_insert = PatriciaNode(ip, rightmost_set_bit, {mask: data})
         to_insert.parent = cur_node
         if is_set(cur_node.bit, ip, v6):
             cur_node.right = to_insert
@@ -171,6 +198,67 @@ class PatriciaTrie:
         # increment the size of the trie due to the added node
         self.size += 1
 
+    def check_value_exists(self, prefix: str) -> (bool, bool):
+        """Check to see if a value exists in the trie already.
+        Returns 2 bools, the first to indicate whether the IP existed in the trie, and the
+        second to indicate whether the mask existed on that IP.
+
+        There can only be one of any IP stored in the trie, if you stored 2 prefixes,
+        "192.168.0.0/16", and "192.168.0.0/24", the data would be stored on the same
+        node, as the IP address "192.168.0.0" is the same. Even though these refer
+        to 2 separate networks.
+        
+        This method exists so you can check to see if this will happen. The first boolean
+        returned indicates if an IP address is already present, for example in the case
+        above, if called after inserting "192.168.0.0/16", this method would return True
+        in the first boolean for a prefix of "192.168.0.0/24" and other netmasks, and False
+        for the second boolean.
+        The second boolean is for checking if the mask is present -- if called after
+        inserting "192.168.0.0/16" with a prefix of "192.168.0.0/16" it would return
+        True for both booleans.
+
+        Args:
+            prefix: The prefix to find in the trie, i.e. "192.168.0.0/16"
+
+        Returns:
+            (bool, bool): A 2-tuple of bools indicating whether the IP existed and the mask existed, respectively.
+
+        Raises:
+            ValueError: When trying to find an IPv4 address in a v6 trie and vice-versa.
+        """
+        # check to see if the prefix is IPv6 and then check whether
+        # or not we can store it given what's already in the trie
+        v6 = is_v6(prefix)
+        if self.v6 and not v6:
+            raise ValueError("Cannot store IPv4 prefix in IPv6 trie")
+        elif not self.v6 and v6 and self.size > 0:
+            raise ValueError("Cannot store IPv6 prefix in IPv4 trie")
+        else:
+            self.v6 = v6
+
+        # parse the CIDR string
+        ip, mask = cidr_atoi(prefix)
+
+        # traverse the trie with the given IP
+        last_node = None
+        for node in self.traverse(prefix):
+            last_node = node
+
+        ip_exists = False
+        mask_exists = False
+
+        if last_node.ip == ip:
+            # if the last node's IP equals this IP, the IP exists
+            # we now need to check if the mask is in the set of masks
+            ip_exists = True
+            mask_exists = mask in last_node.value
+        else:
+            # if the IP didn't exist, we can be sure the mask didn't either
+            ip_exists = False
+            mask_exists = False
+
+        return ip_exists, mask_exists
+
     def find(self, prefix: str) -> PatriciaNode:
         """Find a value in the trie.
 
@@ -178,7 +266,7 @@ class PatriciaTrie:
             prefix: The prefix to find in the trie, i.e. "192.168.0.0/16"
         
         Returns:
-            Any: The data stored in the node if found, None otherwise.
+            PatriciaNode: The node if found, None otherwise.
 
         Raises:
             ValueError: When trying to find an IPv4 address in a v6 trie and vice-versa.
@@ -196,14 +284,32 @@ class PatriciaTrie:
 
         return None
 
+    def find_all_values(self, prefix: str) -> Dict[str, Any]:
+        v6 = is_v6(prefix)
+        if v6 and not self.v6:
+            raise ValueError("Trying to find IPv6 value in IPv4 trie")
+        elif not v6 and self.v6:
+            raise ValueError("Trying to find IPv4 value in IPv6 trie")
+
+        values = {}
+        
+        # for each node on the way down
+        for node in self.traverse(prefix):
+            # get the values from the node and combine them into
+            # the result dictionary
+            vals = node.get_values(prefix)
+            values = {**values, **vals}
+        return values
+
+
     def find_all(self, prefix: str) -> List[PatriciaNode]:
-        """Traverses the trie and returns any values it found.
+        """Traverses the trie and returns any nodes it found.
 
         Args:
             prefix: The prefix to find in the trie, i.e. "192.168.0.0/16"
 
         Returns:
-            List[Any]: The values found when traversing the trie.
+            List[PatriciaNode]: Ordered list of nodes found when traversing the trie.
 
         Raises:
             ValueError: When trying to find an IPv4 address in a v6 trie and vice-versa.
