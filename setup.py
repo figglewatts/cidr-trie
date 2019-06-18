3bbe01e9a5fab23c77b680e8ac4439dfd751cb89
Figglewatts <me@figglewatts.co.uk>
2019-06-18 11:13:54 +0100

    Fixed some of the locks

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return r.read()
 
 setup(name="cidr_trie",
-      version="2.1.3",
+      version="2.1.4",
       description="Store/search CIDR prefixes in a trie structure.",
       long_description=readme(),
       long_description_content_type="text/x-rst",
